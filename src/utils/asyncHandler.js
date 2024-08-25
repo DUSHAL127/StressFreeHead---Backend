94f8100e93554cfd85c78777bbe4637a45742cca
DUSHAL127 <dushal1272001@gmail.com>
2024-08-25 14:57:15 +0530

    register

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
+
+
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+   return (req, res, next) => {
         Promise.resolve(requestHandler(req,res, next))
         .catch((err) => next(err))
     }
@@ -16,13 +18,3 @@ export {asyncHandler}
 
 
 
-// const asyncHandler = (fn) => async (req, res, next)=> {
-//     try{
-//         await fn(req, res, next) //await
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: error.message
-//         })
-//     }
-// }
