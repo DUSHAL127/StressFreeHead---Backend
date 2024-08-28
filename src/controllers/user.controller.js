df353110dcbbf752cec3586d8c647371ff94b435
DUSHAL127 <dushal1272001@gmail.com>
2024-08-28 12:42:48 +0530

    refreshtoken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "./../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -83,7 +83,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const {email, username, password}= req.body;
 
-  if ( !username || !email) {
+  if ( !username && !email) {
     throw new ApiError(400, "email or password is required");
   }
 
@@ -150,4 +150,54 @@ return res
 
 })
 
-export {registerUser, loginUser,logoutUser }
+
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+ const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+ if (incomingRefreshToken) {
+  throw new ApiError(401, "unauthorized request"); 
+ }
+
+try {
+   const decodedToken = jwt.verify(
+    incomingRefreshToken,
+    process.env.REFRESH_TOKEN_SECRET
+  
+  )
+  
+  const user = await User.findById(decodedToken?._id)
+  
+  if (!user) { 
+    throw new ApiError(401, "Invalid refresh token");
+  }
+  
+  if (incomingRefreshToken !== user.refreshToken) {
+    throw new ApiError(401, "Refresh token is expired or used");
+  }
+  
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+  const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+  return res.status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", newRefreshToken, options)
+  .json(
+    new ApiResponse(200, {accessToken, refreshToken: newRefreshToken}, "Access token refreshed successfully")
+  )
+  
+  
+} catch (error) {
+
+  throw new ApiError(401, error?.message || "Invalid refresh token");
+  
+}
+// const accessToken = user.generateAccessToken();
+// return res.status(200).json(new ApiResponse(200, {accessToken}, "Access token refreshed successfully"));
+ 
+
+})
+
+export {registerUser, loginUser,logoutUser, refreshAccessToken }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
@@ -11,4 +11,8 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
+
+
 export default router;
